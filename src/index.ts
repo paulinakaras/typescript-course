997f07c45725ffdc938ad7ba99b3fba8acb6da46
paulinakaras <ppaulina.karas@gmail.com>
2025-03-09 17:34:48 +0000

    Define variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+// Define const
 const firstName: string = "John";
+// firstName = "Ola"; it throws error, const cannot change value
 
 console.log(firstName);
+
+// Define variables
+let secondName: string = "Ala";
+secondName = "Ola"; // let allow to change value
+
+let age: number = 25;
+
+let isAdult: boolean = false;
